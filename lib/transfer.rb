090c4b6cdb6c6090e44c6866dc0e5b9a28e74a0d
Lauren Stone <lstone321@gmail.com>
2020-08-13 00:57:03 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,12 +1,18 @@
 class Transfer
-  
-  attr_accessor :sender, :receiver, :status
-  
-  def initialize(sender)
-    @sender = sender
-    @receiver = receiver 
-    @status = "pending"
-  end 
- 
+  attr_accessor :sender, :receiver, :amount, :status
 
-end
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+    self.valid?
+  end
+
+  def valid?
+    if self.sender != "" && self.receiver != "" && self.amount > 0
+      true
+    else
+      puts "invalid"
+    end
+  end
